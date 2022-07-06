19ee3f02e1656ee0e7eb4a31c0d4944797a88aa4
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2022-07-06 08:16:07 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 export { default as routerPlugin } from "./plugin";
+export {
+  getAppBaseName,
+  getHomePage,
+  getRouterPluginConfig,
+  getRouterView,
+  getRoutes,
+  isHomePage,
+} from "./plugin/selectors";
+export { locationChange, goBack, requestRoute } from "./plugin/actions";
+export { compareRoutes, extractHomePage } from "./plugin/helper";
